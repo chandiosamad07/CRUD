c652e04e1571a6447bba521136f60922d510e17b
chandiosamad07 <absamadchandio@gmail.com>
2023-06-23 00:55:13 +0500

    frontend done

--- Backend/index.js
+++ Backend/index.js
@@ -1,23 +1,19 @@
-const express = require("express");
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose');
 
-const mongoose = require("mongoose");
-require("dotenv").config();
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/CRUD", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
-const routes = require("./routes/TaskRoute");
-
-const cors = require("cors");
-
-const app = express();
-const PORT = process.env.PORT | 5000;
-
-app.use(express.json());
-app.use(cors());
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected..."))
-  .catch((err) => console.log(err));
-
-app.use("/api", routes);
-
-app.listen(PORT, () => console.log(`Listening at ${PORT}`));
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(5000, () => console.log('Example app listening on port 5000!'))
+app.use(express.json())
